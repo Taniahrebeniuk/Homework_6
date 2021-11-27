fd5ca0bcf47f5c918d59af1a8097e7b2b9ede23c
tetianahrebeniuk <taniahrebeniuk@gmail.com>
2021-11-27 16:09:08 +0200

    Homework #6

---
+++ src/Count_words.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Count_words {
+    public static void main(String[] args) {
+      String text;
+      int countWords = 0;
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter string: ");
+      text = scanner.nextLine();
+        for(int i=0; i<text.length()-1; i++) {
+            if(text.charAt(i)==' ' && text.charAt(i+1)!=' ')
+                countWords++;
+        }
+        System.out.println("Total number of words in string are: "+ (countWords+1));
+    }
+}
+
+
